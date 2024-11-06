bf1910c701cdd420bf097a3ec783616700c1641c
Anna Karutina <anna.karutina@khk.ee>
2024-11-06 13:27:51 +0200

    Write data to file

--- index.js
+++ index.js
@@ -7,15 +7,44 @@ const app = express()
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+app.use(express.urlencoded({extended: true}))
+
+const readFile = (filename) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(filename, 'utf-8', (error, data) => {
+            if (error) {
+                console.error(error)
+                return
+            }
+            const tasks = data.split('\n')
+            resolve(tasks)
+        })
+    })
+}
+
 app.get('/', (req, res) => {
-    fs.readFile('./tasks', 'utf-8', (error, data) => {
-        if (error) {
-            console.error(error)
-            return
-        }
-        const tasks = data.split('\n')
+    readFile('./tasks')
+      .then((tasks) => {
         res.render('index', {tasks: tasks})
-    }) 
+    })
+})
+
+app.post('/', (req, res) => {
+    readFile('./tasks')
+      .then((tasks) => {
+        tasks.push(req.body.task)
+        const data = tasks.join('\n')
+        
+        fs.writeFile('./tasks', data, err => {
+            if (err) {
+              console.error(err);
+              return
+            } else {
+              res.redirect('/')
+            }
+        });
+
+    })
 })
 
 app.listen(3001, () => {
